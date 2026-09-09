c5bf41dfa190e8b955147b10a68eaf82fd82ff2c
Mohamed Kamal <elgenegymmm@gmail.com>
2026-09-09 15:10:17 +0300

    Add InfraQTY pipe calculation logic

--- src/InfraQTY.AutoCAD/Commands.cs
+++ src/InfraQTY.AutoCAD/Commands.cs
@@ -1,6 +1,7 @@
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.EditorInput;
+using InfraQTY.Core.Models;
 
 namespace InfraQTY.AutoCAD;
 
@@ -12,6 +13,16 @@ public class Commands
         Document doc = Application.DocumentManager.MdiActiveDocument;
         Editor ed = doc.Editor;
 
-        ed.WriteMessage("\nInfraQTY is running!");
+        PipeItem testPipe = new PipeItem(
+            "PVC",
+            0.30,
+            100,
+            1);
+
+        ed.WriteMessage(
+            $"\nInfraQTY is running!" +
+            $"\nMaterial: {testPipe.Material}" +
+            $"\nDiameter: {testPipe.Diameter}" +
+            $"\nLength: {testPipe.Length} m");
     }
 }
